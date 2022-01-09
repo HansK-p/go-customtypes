565610182a584657d27c47f354d85930daab1317
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2022-01-09 20:27:57 +0100

    Added '2006-01-02' as a valid Date format

--- date_test.go
+++ date_test.go
@@ -10,12 +10,14 @@ import (
 type Dates struct {
 	Rfc3339  Date
 	Custom_1 Date
+	Custom_2 Date
 }
 
 func TestDate(t *testing.T) {
 	yamlDates := `---
 rfc3339: "2021-12-13T23:00:05Z"
 custom_1: "2021-12-13T23:00Z"
+custom_2: "2021-12-13"
 `
 	t.Log("Parsing the yaml file")
 	dates := Dates{}
@@ -24,7 +26,12 @@ custom_1: "2021-12-13T23:00Z"
 	}
 	rfc3339 := time.Time(dates.Rfc3339)
 	custom_1 := time.Time(dates.Custom_1)
+	custom_2 := time.Time(dates.Custom_2)
+
 	if rfc3339.Sub(custom_1).Seconds() != 5 {
 		t.Errorf("There isn't 5 seconds difference as expected between dates, %s - %s = %f", rfc3339, custom_1, rfc3339.Sub(custom_1).Seconds())
+	}
+	if custom_1.Sub(custom_2).Seconds() != 3600*23 {
+		t.Errorf("There isn't 3600*23 seconds difference as expected between dates, %s - %s = %f", custom_1, custom_2, custom_1.Sub(custom_2).Seconds())
 	}
 }
